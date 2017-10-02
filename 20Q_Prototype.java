fb4d5e7d534c9d01a28ec3f61e32380bdc6717b7
Juan Burdick <burdick@andrews.edu>
2017-10-01 22:00:14 -0400

    JSON working?

--- 20Q_Prototype.java
+++ 20Q_Prototype.java
@@ -20,11 +20,14 @@ import java.util.Scanner;
 import java.io.*;
 
 public class 20Q_Prototype
-{}
+{
 
-    public static void main (String[] args) 
+    public static void main (String[] args) throws IOException
     {
         private int count = 0;
+        FileInputStream qDoc = null;
+        FileInputStream aDocIn = null;
+        FileOutputStream aDocOut = null;
         File file = null;
         private String input;
         private int subsetSize = 0;
@@ -33,51 +36,58 @@ public class 20Q_Prototype
         private ArrayList() answerArray;
         private ArrayList() questionSubset;
         Scanner scan = new Scanner(System.in);
-        // input variable for yes or no answer @param scan
+        JSONParser parser = new JSONParser();
+        // input variable for yes or no answer
         // scan = input.next(); 
         
-    }
-    
-    public void runTime() 
-    {
-        while (count >= 0 || count <= 20) 
+        qDoc = new FileInputStream("questions.txt");
+        aDocIn = new FileInputStream("answers.txt");
+        aDocOut = new FileOutputStream("answers.txt");
+        
+        try 
         {
-            system.out.println(file.count)
-            // read question from file
-            // receiveInput -> input
-            scan = input.next();
-            input.toUpperCase();
-            while !(input == "Y" || input == "YES" || input == "N" || input == "NO") 
-            { 
-                system.out.println("ERROR: input yes or no");
-            }
-            if (input == "Y" || input == "YES") 
+            Object obj = parser.parse(new FileReader("questions.txt"));
+            JSONObject jsonObject = (JSONObject) obj;
+
+            while (count >= 0 || count <= 20) 
             {
-                // get size of subset somehow
-                //questionID = selectNextQuestion(questionID.YesSubQuestions[], subsetSize);
-                //TagArray[count] = questionID.Tag[0];
-                TagArray[count] = "Q" + count + " = yes";
+                system.out.println(file.count)
+                // read question from file
+                // receiveInput -> input
+                scan = input.next();
+                input.toUpperCase();
+                while !(input == "Y" || input == "YES" || input == "N" || input == "NO") 
+                { 
+                    system.out.println("ERROR: input yes or no");
+                }
+                if (input == "Y" || input == "YES") 
+                {
+                    // get size of subset somehow
+                    //questionID = selectNextQuestion(questionID.YesSubQuestions[], subsetSize);
+                    //TagArray[count] = questionID.Tag[0];
+                    TagArray[count] = "Q" + count + " = yes";
+                }
+                else if (input == "N" || input == "NO") 
+                {
+                    //get size of subset somehow
+                    //questionID = selectNextQuestion(questionID.NoSubQuestions[], subsetSize);
+                    //TagArray[count] = questionID.Tag[1];
+                    TagArray[count] = "Q" + count + " = no";
+                }
+                else { break; }
+                if (count == 20) 
+                { 
+                    populateAnswers(TagArray[]);
+                }
+                else if (count > 20) { break; }
+                count++;
             }
-            else if (input == "N" || input == "NO") 
-            {
-                //get size of subset somehow
-                //questionID = selectNextQuestion(questionID.NoSubQuestions[], subsetSize);
-                //TagArray[count] = questionID.Tag[1];
-                TagArray[count] = "Q" + count + " = no";
-            }
-            else 
-            { 
-                break; 
-            }
-            if (count == 20) 
-            { 
-                populateAnswers(TagArray[]);
-            }
-            else if (count > 20)
-            {
-                break;
-            }
-            count++;
+        }
+        finally
+        {
+            if (qDoc != null) { qDoc.close(); }
+            if (aDocIn != null) { aDocIn.close(); }
+            if (aDocOut != null) { aDocOut.close(); }
         }
     }
 
