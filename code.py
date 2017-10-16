a0c7d13a6f95029be715c81f0fcb2b48e7b171e3
Juan Burdick <burdick@andrews.edu>
2017-10-16 12:22:09 -0400

    writes wrong guesses to answer doc

--- code.py
+++ code.py
@@ -2,57 +2,70 @@ import json
 import random
 
 # examples for making and packing questions
-print("20q project")
+print("Welcome to my home. I believe " +
+      "that I can guess what you are thinking.")
 
-# array to store question tags
+# list to store question tags to access correct answers
 tagList = [0]*20
-answerList = []
-alreadyAsked = []
-qIndex = 0
-# reads file.txt and stores question dict @param data
+answerList = [] # list to store possible answers pulled from doc
+alreadyAsked = [] # list of question IDs to prevent asking same q
+qIndex = 0 # index of currently accessed question
+
+# reads file.txt and stores question data in a dict
 with open('file.txt', 'r') as fileQ:
     questions = json.load(fileQ)
 print(questions)
-# reads answer.txt and stores answers dict
+# reads answer.txt and stores answer data in a dict
 with open('answer.txt', 'r') as fileA:
     answers = json.load(fileA)
 print(answers)
+# sets up a new dict with same parameters to have an editable
+# dict with the answers
 possibleAnswers = answers
-
-
-
+# iterative values
 count = 0
 a = 1
 
-
+########################################################
 
 # main while loop of program that asks questions
-while count < 5:
-    for x in range(1):
-        qIndex = random.randint(1, len(questions))
-    while qIndex in alreadyAsked:
+while count < 20:
+    if count == 5:
+        # generates a random index for a question to be accessed
         for x in range(1):
             qIndex = random.randint(1, len(questions))
-    alreadyAsked.append(qIndex)
+        # ensures that the random index hasn't already been asked
+        # if it has, re-generate a number
+        while qIndex in alreadyAsked:
+            for x in range(1):
+                qIndex = random.randint(1, len(questions))
+        alreadyAsked.append(qIndex) # add index to already asked list
+    elif count
+
     questionCurrent = (questions["question" + str(qIndex)])
     print(questionCurrent["question"])
-    cond = 1
+    cond = 1 # variable to ensure correct input
     while cond != 0:
         answer = input("")
         if answer == "yes":
+            # adds yesTag associated with question to list
             tagList[count] = questionCurrent["yTag"]
             cond = 0
         elif answer == "no":
+            # adds noTag associated with question to list
             tagList[count] = questionCurrent["nTag"]
             cond = 0
         else:
             print("please enter 'yes' or 'no' ")
     count += 1
 
+# randomly selects an answer in array to guess
 for y in range(1):
     index = random.randint(0, len(answers))
 guess = (answers["ID" + str(0)])
 print("Was it " + guess["name"])
+
+# checks to see if guess was correct, and ensures correct input
 cond = 1
 while cond != 0:
     correct = input("")
@@ -64,12 +77,16 @@ while cond != 0:
     else:
         print("please enter 'yes' or 'no' ")
 
+# adds answer to array if guess was wrong
 if cond == 0:
+    # gets animal from user
     print("What was the animal?")
     rightAnimal = input("")
+    # creates a dict with the string from user and the existing tagList
+    animal = {"name" : rightAnimal, "tag" : tagList}
+    animalID = "ID" + str(len(answers))
+    answerToWrite = {animalID : animal}
+    # appends new answer to the existing file
     with open('answer.txt', 'a') as fileA:
-        fileA.write(json.dumps(questions))
-
-# makes questions with tagList and animal
-    # needs if statement to decide if answer
-    # exists or needs creation
+        fileA.write(json.dumps(answerToWrite))
+    print("Darn! You got me. Thanks for playing!")
