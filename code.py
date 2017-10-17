73c2f0e0657b0eba525d0a825e6fe2c4d666df2c
burdick <juan@burdick.ws>
2017-10-16 22:33:34 -0400

    idek

--- code.py
+++ code.py
@@ -1,37 +1,33 @@
 import json
 import random
 
-tagList = [0]*20  # list to store question tags to access correct answers
-answerList = []  # list to store possible answers pulled from doc
-alreadyAsked = []  # list of question IDs to prevent asking same q
-qIndex = 0  # index of currently accessed question
-iterator = 0  # iterative value
-allTags = []  # allTags, loaded to check most common tag in answers
-tagRarity = []  # sibling to allTags, contains rarity of each tag
-tagIterator = 0  # iterator for filling allTags
-
 # reads file.txt and stores question data in a dict
 with open('file.txt', 'r') as fileQ:
     questions = json.load(fileQ)
 print(questions)
+# sets up a new dict with same parameters to have an editable dict with the questions
+unaskedQuestions = dict(questions)
 # reads answer.txt and stores answer data in a dict
 with open('answer.txt', 'r') as fileA:
     answers = json.load(fileA)
 print(answers)
-# sets up a new dict with same parameters to have an editable
-# dict with the answers
-possibleAnswers = answers
+# sets up a new dict with same parameters to have an editable dict with the answers
+possibleAnswers = dict(answers)
 
-# populate allTag list
-while tagIterator < len(questions):
-    curQ = questions["question" + str(tagIterator + 1)]
-    allTags.append(curQ["yTag"])
-    tagRarity.append(0)
-    allTags.append(curQ["nTag"])
-    tagRarity.append(0)
-    tagIterator += 1
-print(allTags)
-tagIterator = 0
+tagList = [0]*20  # list to store question tags to access correct answers
+answerList = []  # list to store possible answers pulled from doc
+alreadyAskedID = []  # list of question IDs to prevent asking same q
+alreadyAsked_yTag = []  # list of tags already asked
+alreadyAsked_yTagRarity = []  # list for rarity of already asked tags
+qIndex = 0  # index of currently accessed question
+iterator = 0  # iterative value
+all_yTags = []  # allTags, loaded to check most common tag in answers
+all_nTags = []  # allTags, loaded to check most common tag in answers
+yTagRarity = []  # sibling to allTags, contains rarity of each tag
+nTagRarity = []  # sibling to allTags, contains rarity of each tag
+tagIterator = 0  # iterator for filling allTags
+aIndex = 0  # index of answer to guess
+temp = {}  # for data transfer
 
 ########################################################
 
@@ -40,51 +36,118 @@ print("Welcome to my home. I believe " +
 # main while loop of program that asks questions
 while iterator < 20:
     # randomly generate the first 5 questions to get a baseline
-    if iterator < 5:
+    if iterator < 2:
         # generates a random index for a question to be accessed
         for x in range(1):
-            qIndex = random.randint(1, len(questions))
-        # ensures that the random index hasn't already been asked
-        # if it has, re-generate a number
-        while qIndex in alreadyAsked:
-            for x in range(1):
-                qIndex = random.randint(1, len(questions))
-        alreadyAsked.append(qIndex)  # add index to already asked list
-    # intelligently select the rest
-    elif 5 <= iterator < 20:
-        # generate the rarity of all tags
-        while tagIterator < len(answers):
-            ans = answers["ID" + str(tagIterator)]
-            subTagIterator = 0
-            while subTagIterator < len(allTags):
-                # only increment rarity if the tag appears
-                if allTags[subTagIterator] in ans["tag"]:
-                    tagRarity[subTagIterator] += 1
-                subTagIterator += 1  # move subIterator
-            tagIterator += 1  # move iterator
+            qIndex = random.randint(1, len(unaskedQuestions))
 
-        tagIterator = 0  # reset iterator so it can be reused
-        indexOfMostCommon = 0  # used to keep track of the most common tag
-        # find the index of the most common tag
-        # so we can access it from the allTags list
-        while tagIterator < len(tagRarity):
-            if tagRarity[tagIterator] > indexOfMostCommon:
-                indexOfMostCommon = tagRarity[tagIterator]
-            tagIterator += 1  # move iterator
+    # intelligently select the remaining questions
+    elif 2 <= iterator < 20:
 
-        tagIterator = 1  # reset iterator so it can be reused
-        # get the question associated with the most common tag
-        while tagIterator < (len(questions) + 1):
-            curQ = questions["question" + str(tagIterator)]
-            if allTags[indexOfMostCommon] == curQ["yTag"]:
-                qIndex = tagIterator
-            elif allTags[indexOfMostCommon] == curQ["nTag"]:
-                qIndex = tagIterator
-            tagIterator += 1  # move iterator
+        # TODO: ensure most common tag question doesn't get re-asked
 
-    questionCurrent = (questions["question" + str(qIndex)])
-    print(questionCurrent["question"])
+        tagIterator = 1  # reset for use
+        temp.clear()  # clear for a new data set
+        # add all questions to temp except the one we just saw
+        while tagIterator <= len(possibleAnswers):
+            a = possibleAnswers["ID" + str(tagIterator)]
+            if a is not questionCurrent:
+                qTemp = {"question" + str(tagIterator): q}
+                temp.update(qTemp)
+            tagIterator += 1
+        unaskedQuestions = dict(temp)
+
+
+
+        tagIterator = 1  # reset iterator for use
+        subTagIterator = 0  # reset iterator for use
+        loopPart = 1  # used to determine which subsection of loop to be operating
+        temp.clear()
+        # update the running list of possible answers
+        while tagIterator <= len(possibleAnswers):
+            ans = possibleAnswers["ID" + str(tagIterator)]
+            delValue = {}
+
+            if (tagIterator < len(possibleAnswers)) and (loopPart == 1):
+                strike = 0
+                # check each answer for all tags in tagList
+                # pop and break if there are more than 4 errors
+                while subTagIterator < len(tagList):
+                    tag = tagList[subTagIterator]
+                    if strike >= 4:
+                        aTemp = {"ID" + str(subTagIterator)}
+                        delValue.update(aTemp)
+                        break
+                    elif tag not in ans["tag"]:
+                        strike += 1
+                    subTagIterator += 1
+                tagIterator += 1
+                if tagIterator == len(possibleAnswers):
+                    loopPart = 2
+            if (tagIterator < len(possibleAnswers)) and (loopPart == 2):
+                subTagIterator = 0
+                while subTagIterator < len(delValue):
+                    toDelete = delValue["ID" + str(subTagIterator)]
+                    if ans is not toDelete:
+                        aTemp = {"ID" + str(tagIterator): ans}
+                        temp.update(aTemp)
+                    subTagIterator += 1
+                tagIterator += 1
+
+
+
+        tagIterator = 0  # reset for use
+        # populate allTag list
+        while tagIterator < len(unaskedQuestions):
+            curQ = unaskedQuestions["question" + str(tagIterator + 1)]
+            all_yTags.append(curQ["yTag"])
+            yTagRarity.append(0)
+            tagIterator += 1
+        print(all_yTags)
+        print(yTagRarity)
+
+        while True:
+            tagIterator = 0  # reset for use
+            # generate the rarity of all tags
+            while tagIterator < len(possibleAnswers):
+                ans = possibleAnswers["ID" + str(tagIterator + 1)]
+                subTagIterator = 0  # reset for use
+                while subTagIterator < len(all_yTags):
+                    # only increment rarity if the tag appears
+                    if all_yTags[subTagIterator] in ans["tag"]:
+                        yTagRarity[subTagIterator] += 1
+                    subTagIterator += 1  # move subIterator
+                tagIterator += 1  # move iterator
+
+            tagIterator = 0  # reset iterator so it can be reused
+            indexOfMostCommon = 0  # used to keep track of the most common tag
+            greatest = 0  # used to find the most common tag
+            # find the index of the most common tag
+            # so we can access it from the allTags list
+            while tagIterator < len(yTagRarity):
+                if yTagRarity[tagIterator] > greatest:
+                    greatest = yTagRarity[tagIterator]
+                    indexOfMostCommon = tagIterator
+                tagIterator += 1  # move iterator
+
+            tagIterator = 1  # reset iterator so it can be reused
+            # get the question associated with the most common tag
+            while tagIterator <= len(unaskedQuestions):
+                curQ = unaskedQuestions["question" + str(tagIterator)]
+                if all_yTags[indexOfMostCommon] == curQ["yTag"]:
+                    qIndex = tagIterator
+                tagIterator += 1  # move iterator
+
+            # break if condition is met
+            if qIndex not in alreadyAskedID:
+                break
+
+        alreadyAskedID.append(qIndex)
+
+    questionCurrent = (unaskedQuestions["question" + str(qIndex)])
+    print(str(iterator + 1) + ". " + questionCurrent["question"])
     cond = 1  # variable to ensure correct input
+    # force correct input
     while cond != 0:
         answer = input("")
         answer.lower()
@@ -98,12 +161,25 @@ while iterator < 20:
             cond = 0
         else:
             print("please enter 'yes' or 'no' ")
+
+    tagIterator = 1  # reset for use
+    temp.clear()  # clear for a new data set
+    # add all questions to temp except the one we just saw
+    while tagIterator <= len(unaskedQuestions):
+        q = unaskedQuestions["question" + str(tagIterator)]
+        if q is not questionCurrent:
+            qTemp = {"question" + str(tagIterator) : q}
+            temp.update(qTemp)
+        tagIterator += 1
+    unaskedQuestions = dict(temp)
+
+
     iterator += 1
 
 # randomly selects an answer in array to guess
 for y in range(1):
-    index = random.randint(0, len(answers))
-guess = (answers["ID" + str(0)])
+    aIndex = random.randint(0, len(answers))
+guess = (answers["ID" + str(aIndex)])
 print("Was it " + guess["name"])
 
 # checks to see if guess was correct, and ensures correct input
@@ -125,11 +201,22 @@ if cond == 0:
     print("What was the animal?")
     rightAnimal = input("")
     rightAnimal.lower()
+    tagIterator = 1
+    exists = 0  # variable to avoid adding a duplicate animal
+    # check to see if answer already exists, and update it if so
+    while tagIterator <= len(answers):
+        ans = answers["ID" + str(tagIterator)]
+        if rightAnimal == ans["name"]:
+            animal = {"tag": tagList}
+            ans.update(animal)
+            exists = 1  # animal already exists, don't add a new one
+        tagIterator += 1  # move iterator
     # creates a dict with the string from user and the existing tagList
-    animal = {"name" : rightAnimal, "tag" : tagList}
-    animalID = "ID" + str(len(answers))
-    answerToWrite = {animalID : animal}
-    answers.update(answerToWrite)
+    if exists == 0:
+        animal = {"name" : rightAnimal, "tag" : tagList}
+        animalID = "ID" + str(len(answers))
+        answerToWrite = {animalID : animal}
+        answers.update(answerToWrite)
     # appends new answer to the existing file
     with open('answer.txt', 'w') as fileW:
         fileW.write(json.dumps(answers))
--- writer.py
+++ writer.py
@@ -1,27 +1,34 @@
 import json
 
 # question dicts
-q1 = {"question": "does it have fur", "yTag": "fur", "nTag": "nofur"}
-q2 = {"question": "does it eat meat", "yTag": "carni/omni", "nTag": "herbivore"}
-q3 = {"question": "does it have a tail", "yTag": "tail", "nTag": "notail"}
-q4 = {"question": "dies it fly", "yTag": "fly", "nTag": "notfly"}
-q5 = {"question": "does it live in water", "yTag": "water", "nTag": "land/air"}
-q6 = {"question": "does it live on land", "yTag": "land", "nTag": "water/air"}
-q7 = {"question": "is it a reptile", "yTag": "reptile", "nTag": "notreptile"}
-q8 = {"question": "does it a mammal", "yTag": "mammal", "nTag": "notmammal"}
-q9 = {"question": "can it climb trees", "yTag": "climber", "nTag": "noclimb"}
-q10 = {"question": "does it breath water", "yTag": "gills", "nTag": "no gills"}
-q11 = {"question": "is it bigger than or the same size as a dog", "yTag": "biggerthandog", "nTag": "smallerthandog"}
-q12 = {"question": "is it active at night", "yTag": "nocturnal", "nTag": "daytime"}
-q13 = {"question": "does it live primarily in africa", "yTag": "africa", "nTag": "notafrica"}
-q14 = {"question": "is it domesticated", "yTag": "domestic", "nTag": "wild"}
-q15 = {"question": "is it a herbavore", "yTag": "herbivore", "nTag": "carni/omni"}
-q16 = {"question": "can it shed skin", "yTag": "shedSkin", "nTag": "noShedSkin"}
-q17 = {"question": "can it ride a skateboard", "yTag": "rideskateboard", "nTag": "noskateboard"}
-q18 = {"question": "does it live in the us", "yTag": "unitedstates", "nTag": "notunitedstates"}
-q19 = {"question": "does it have a good sense of smell", "yTag": "goodatsmell", "nTag": "badatsmell"}
-q20 = {"question": "does it have echo location", "yTag": "echolocator", "nTag": "noecholocator"}
-q21 = {"question": "is it usually around snow", "yTag": "snow", "nTag": "notSnow"}
+q1 = {"question": "Does it have fur?", "yTag": "fur", "nTag": "noFur"}
+q2 = {"question": "Does it eat meat?", "yTag": "carnivore/omnivore", "nTag": "herbivore"}
+q3 = {"question": "Does it have a tail?", "yTag": "tail", "nTag": "noTail"}
+q4 = {"question": "Can it fly?", "yTag": "canFly", "nTag": "cantFly"}
+q5 = {"question": "Does it mostly live in water?", "yTag": "water", "nTag": "land/air"}
+q6 = {"question": "Is it an amphibian?", "yTag": "amphibian", "nTag": "notAmphibian"}
+q7 = {"question": "Is it a reptile?", "yTag": "reptile", "nTag": "notReptile"}
+q8 = {"question": "Is it a mammal?", "yTag": "mammal", "nTag": "notMammal"}
+q9 = {"question": "Can it climb trees?", "yTag": "climbsTrees", "nTag": "notClimbTrees"}
+q10 = {"question": "Does it breath water?", "yTag": "gills", "nTag": "notGills"}
+q11 = {"question": "Is it bigger than a dog?", "yTag": "biggerThanDog", "nTag": "smallerThanDog"}
+q12 = {"question": "Is it active at night?", "yTag": "nocturnal", "nTag": "daytime"}
+q13 = {"question": "Does it live primarily in Africa?", "yTag": "africa", "nTag": "notAfrica"}
+q14 = {"question": "Is it domesticated?", "yTag": "domestic", "nTag": "wild"}
+q15 = {"question": "Does it have hooves?", "yTag": "hooves", "nTag": "notHooves"}
+q16 = {"question": "Can it shed its skin?", "yTag": "shedSkin", "nTag": "noShedSkin"}
+q17 = {"question": "Can it fit on a skateboard?", "yTag": "fitsOnSkateboard", "nTag": "doesntFitOnSkateboard"}
+q18 = {"question": "Does it live primarily in North America?", "yTag": "inNA", "nTag": "notInNA"}
+q19 = {"question": "Does it have a good sense of smell?", "yTag": "goodAtSmell", "nTag": "badAtSmell"}
+q20 = {"question": "Does it have echo location?", "yTag": "echoLocation", "nTag": "noEchoLocation"}
+q21 = {"question": "Is it native to snowy regions?", "yTag": "snow", "nTag": "notSnow"}
+q22 = {"question": "Is it a fish?", "yTag": "fish", "nTag": "notFish"}
+q23 = {"question": "Is it a bird?", "yTag": "bird", "nTag": "notBird"}
+q24 = {"question": "Can it swim?", "yTag": "swim", "nTag": "cantSwim"}
+q25 = {"question": "Does it have legs?", "yTag": "legs", "nTag": "notLegs"}
+
+
+
 
 
 
@@ -30,7 +37,9 @@ questions = {"question1": q1, "question2": q2, "question3": q3, "question4": q4,
              "question5": q5, "question6": q6, "question7": q7, "question8": q8,
              "question9": q9, "question10": q10, "question11": q11, "question12": q12,
              "question13": q13, "question14": q14, "question15": q15, "question16": q16,
-             "question17": q17, "question18": q18, "question19": q19, "question20": q20}
+             "question17": q17, "question18": q18, "question19": q19, "question20": q20,
+             "question21": q21, "question22": q22, "question23": q23, "question24": q24,
+             "question25": q25}
 
 print(questions)
 
